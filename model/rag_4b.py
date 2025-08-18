e714fc5df8df0abd76c2e86bb494e37f562cf262
NguyenPhuongDng <144362042+NguyenPhuongDng@users.noreply.github.com>
2025-08-18 22:12:58 +0700

    hmm

--- model/rag_4b.py
+++ model/rag_4b.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import requests
 from conversation_manager import conversation_manager
 from langchain.prompts import PromptTemplate
+import sys
+sys.stdout.reconfigure(encoding="utf-8")
 
 # Thiết bị
 device = "cuda" if torch.cuda.is_available() else "cpu"
